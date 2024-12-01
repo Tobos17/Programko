431aeb8f97d0e596a9200642a4a7a762c6f52850
Tobos17 <stoulil.tobias@gmail.com>
2024-12-01 23:57:32 +0100

    pkls

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,219 +1,126 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
-namespace SimulationProject
+namespace Soubory
 {
-    class Auto
+    public class TextAnalyzer : StreamReader
     {
-        public int nosnost; 
-        public int dobaCesty;
-        public int dobaNakládání; 
-        public int dobaVykládání; 
+    private readonly Dictionary<string, int> _words;
+    public int WordCount { get; private set; }
+    public int CharactersNoSpacesCount { get; private set; }
+    public int CharactersCount { get; private set; }
 
-        public Auto(int n, int dC, int dN, int dV)
-        {
-            nosnost = n;
-            dobaCesty = dC;
-            dobaNakládání = dN;
-            dobaVykládání = dV;
-        }
-        
-        //pro seřazení v PriorityQueue podle nejdřív nosnosti (čím víc tím líc => -nosnost jako priorita) a pak podle časů (dobaCesty, dobaNakládání, dobaVykládání)
-        public Tuple<int, int, int, int> GetPriority()
-        {
-            return Tuple.Create(-nosnost, dobaCesty, dobaNakládání, dobaVykládání);
-        }
+    // Konstruktor
+    public TextAnalyzer(string fileName) : base(fileName)
+    {
+        _words = new Dictionary<string, int>();
+        AnalyzeFile();
     }
 
-    class Akce
+    // Analýza souboru
+    private void AnalyzeFile()
     {
-        public int jmeno;
-        public TUdalost typUdalosti;
-        public int zacatek;
-
-        public Akce(int id, TUdalost typ, int z)
+        try
         {
-            jmeno = id;
-            typUdalosti = typ;
-            zacatek = z;
-        }
-
-        
-        public enum TUdalost { Naloz, Vyloz }
-    }
-
-    class Simulace
-    {
-        private List<Akce> KalendarAkci = new List<Akce>();
-        private PriorityQueue<Auto, Tuple<int, int, int, int>> Auta = new PriorityQueue<Auto, Tuple<int, int, int, int>>();
-
-        public void Spustit(int písek, List<Auto> seznamAut)
-        {
-            // Přidání aut do prioritní fronty a do kalendáře jeho akci (podle jmena auta)
-            foreach (Auto auto in seznamAut)
-            {   
-                // zařazení do queue podle priorit
-                Auta.Enqueue(auto, auto.GetPriority());
-                KalendarAkci.Add(new Akce(seznamAut.IndexOf(auto), Akce.TUdalost.Naloz, 0));
-            }
-
-            int časSimulace = 0;
-            int posledníNaložení = 0; // čas pro výpočet čekání v queue
-
-            while (písek > 0 && KalendarAkci.Any())
+            string? line;
+            while ((line = ReadLine()) != null)
             {
-                // Kvůli seřazení => začínáme s akcí s nejmenším časem začátku 
-                KalendarAkci = KalendarAkci.OrderBy(a => a.zacatek).ToList();
-                Akce aktuálníAkce = KalendarAkci.First();
-                KalendarAkci.RemoveAt(0);
-                
-                // musím najít akci pro dané auto
-                Auto aktuálníAuto = seznamAut[aktuálníAkce.jmeno];
-                časSimulace = aktuálníAkce.zacatek;
+                CharactersCount += line.Length + Environment.NewLine.Length;
+                CharactersNoSpacesCount += line.Count(c => !char.IsWhiteSpace(c));
 
-                if (aktuálníAkce.typUdalosti == Akce.TUdalost.Naloz)
+                var words = line.Split(new[] { ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                WordCount += words.Length;
+
+                foreach (var word in words)
                 {
-                    // Kontrola, zda auto musí čekat na předchozí nakládku (čas čekání v queue)
-                    int startNaložení = Math.Max(časSimulace, posledníNaložení);
-                    // Console.WriteLine($"{časSimulace} {Math.Max(časSimulace, posledníNaložení)}");
-
-
-                    //zápis události
-                    Console.WriteLine($"Čas {startNaložení}: Auto s {aktuálníAuto.nosnost}kg začíná nakládat.");
-
-                    // Nastavíme čas posledního naložení na čas, kdy toto auto dokončí nakládání (aby auto v pořádí vědelo, kolik má čekat)
-                    // pomocí Math.Max(časSimulace, posledníNaložení) se zjistí jestli bude čekat nebo přijelo do volné queue
-                    posledníNaložení = startNaložení + aktuálníAuto.dobaNakládání;
-
-                    // Přidej událost vyložení
-                    KalendarAkci.Add(new Akce(aktuálníAkce.jmeno, Akce.TUdalost.Vyloz, posledníNaložení + aktuálníAuto.dobaCesty));
-                }
-                else if (aktuálníAkce.typUdalosti == Akce.TUdalost.Vyloz)
-                {
-                    //zápis události
-                    Console.WriteLine($"Čas {časSimulace}: Auto s {aktuálníAuto.nosnost}kg začíná vykládat.");
-
-                    //odeberu
-                    písek -= aktuálníAuto.nosnost;
-
-                    // Pokud je ještě nějaký písek k vykládání, musí se počkat, dokud nebude vše přepraveno
-                    if (písek > 0)
-                    {
-                        // Přidáváme událost nakládání po vykládání
-                        int časDokončeníVykládání = časSimulace + aktuálníAuto.dobaVykládání;
-                        KalendarAkci.Add(new Akce(aktuálníAkce.jmeno, Akce.TUdalost.Naloz, časDokončeníVykládání + aktuálníAuto.dobaCesty));
-                    }
+                    if (_words.ContainsKey(word))
+                        _words[word]++;
                     else
-                    {
-                        // Pokud už není písek, simulace končí, protože všechny akce jsou dokončeny
-                        Console.WriteLine($"Čas dovozu všeho písku: {časSimulace + aktuálníAuto.dobaVykládání} min.");
-                        break;
-                    }
+                        _words[word] = 1;
                 }
             }
-
-           
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Input File Error", ex);
+        }
+        finally
+        {
+            Close();
         }
     }
 
-    internal class Program
+    // Výpis slov oddělených mezerou
+    public string GetWordsSeparatedBySpace()
     {
-        static void Main(string[] args)
-        {   
-            // Simulace končí v okamžik kdy jakékoliv auto vyloží písek, přičemž dosháne zvolené hmotnosti písku
-            // vypisuju jednotlivé události a jejich časy
+        try
+        {
+            BaseStream.Seek(0, SeekOrigin.Begin);
+            DiscardBufferedData();
 
-
-            // s inputem =>
-            // Console.WriteLine("Zvol si hmotnost písku na převoz?");
-            // int pisek = Convert.ToInt32(Console.ReadLine());
-
-            // List<Auto> seznamAut = new List<Auto>();
-
-            // Console.WriteLine("Kolik aut chcete zadat?");
-            // int pocetAut = Convert.ToInt32(Console.ReadLine());
-
-            // for (int i = 0; i < pocetAut; i++)
-            // {
-            //     Console.WriteLine($"Zadejte parametry pro auto {i + 1} ve formátu: nosnost doba_cesty doba_nakladani doba_vykladani");
-            //     string[] vstup = Console.ReadLine().Split(' ');
-            //     int nosnost = int.Parse(vstup[0]);
-            //     int dobaCesty = int.Parse(vstup[1]);
-            //     int dobaNakladani = int.Parse(vstup[2]);
-            //     int dobaVykladani = int.Parse(vstup[3]);
-
-            //     seznamAut.Add(new Auto(nosnost, dobaCesty, dobaNakladani, dobaVykladani));
-            // }
-
-            //
-
-            // s defaultnimi hodnotami
-            // int pisek = 1000;
-
-            // List<Auto> seznamAut = new List<Auto>
-            // {
-            //     new Auto(10, 20, 55, 50), 
-            //     new Auto(45, 28, 15, 254),
-            //     new Auto(5, 50, 94, 29),
-            //     new Auto(65, 29, 15, 25),
-            //     new Auto(15, 55, 14, 59),
-            //     new Auto(145, 20, 45, 245),
-            //     new Auto(154, 23, 16, 95),
-            // };
-
-            //
-
-
-            // input s různým počtem aut: od určitého počtu aut se čas simulace už nezrychluje
-
-            int pisek = 1000;
-
-            Simulace simulace = new Simulace();
-            
-            List<Auto> seznamAut = new List<Auto>
+            string result = "";
+            string? line;
+            while ((line = ReadLine()) != null)
             {
-                new Auto(10, 20, 55, 50), 
-            };
-            simulace.Spustit(pisek, seznamAut);
-
-            List<Auto> seznamAut2 = new List<Auto>
-            {
-                new Auto(10, 20, 55, 50), 
-                new Auto(10, 20, 55, 50), 
-            };
-            simulace.Spustit(pisek, seznamAut2);
-
-            List<Auto> seznamAut3 = new List<Auto>
-            {
-                new Auto(10, 20, 55, 50), 
-                new Auto(10, 20, 55, 50), 
-                new Auto(10, 20, 55, 50), 
-            };
-            simulace.Spustit(pisek, seznamAut3);
-
-            List<Auto> seznamAut4 = new List<Auto>
-            {
-                new Auto(10, 20, 55, 50), 
-                new Auto(10, 20, 55, 50), 
-                new Auto(10, 20, 55, 50), 
-                new Auto(10, 20, 55, 50), 
-            };
-            simulace.Spustit(pisek, seznamAut4);
-
-            List<Auto> seznamAut5 = new List<Auto>
-            {
-                new Auto(10, 20, 55, 50), 
-                new Auto(10, 20, 55, 50), 
-                new Auto(10, 20, 55, 50), 
-                new Auto(10, 20, 55, 50), 
-                new Auto(10, 20, 55, 50), 
-            };
-            simulace.Spustit(pisek, seznamAut5);
-          
-        
-            Console.ReadLine();
-
+                var words = line.Split(new[] { ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                result += string.Join(" ", words) + Environment.NewLine;
+            }
+            return result.TrimEnd();
+        }
+        catch
+        {
+            throw new Exception("Input File Error");
         }
     }
+
+    // Výpis slov a jejich četností
+    public Dictionary<string, int> GetWordFrequencies() => _words;
+}
+
+
+    class Program
+    {
+        static void Main()
+        {
+            string inputFileName = "./bin/Debug/net9.0/0_vstup.txt";
+            string outputFileName = "./bin/Debug/net9.0/0_vystup.txt";
+
+            try
+            {
+                using var analyzer = new TextAnalyzer(inputFileName);
+
+                // Získání údajů
+                int wordCount = analyzer.WordCount;
+                int charCountNoSpaces = analyzer.CharactersNoSpacesCount;
+                int charCount = analyzer.CharactersCount;
+                var wordFrequencies = analyzer.GetWordFrequencies();
+                string wordsSeparatedBySpace = analyzer.GetWordsSeparatedBySpace();
+
+                // Zápis do výstupního souboru
+                using var writer = new StreamWriter(outputFileName);
+                writer.WriteLine($"Počet slov: {wordCount}");
+                writer.WriteLine($"Počet znaků (bez mezer): {charCountNoSpaces}");
+                writer.WriteLine($"Počet znaků (s mezerami): {charCount}");
+        
+                foreach (var kvp in wordFrequencies)
+                {
+                    writer.WriteLine($"{kvp.Key}: {kvp.Value}");
+                }
+                writer.WriteLine();
+                writer.WriteLine("Slova oddělená mezerou:");
+                writer.WriteLine(wordsSeparatedBySpace);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine($"Soubor jménem {inputFileName} neexistuje.");
+            }
+            catch
+            {
+                Console.WriteLine("Input File Error");
+            }
+        }
+    }
+
 }
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f76c19e13c9524e266df4dc41ac6308262c490a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18a1dd8e5f3e896e13cdf58d1654040f743e0c09")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
